4e54e43707fb35c9f7d4e02cf43f8fe75e947737
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-07-09 21:21:36 -0600

    updating script and adding files into assets folder

--- script.js
+++ script.js
@@ -1,57 +1,9 @@
+
+// ------- GLOBAL VARIABLES ---------------
 var city = "";
 var apiKey = "31bc0639ecbf46fe8fb7a18255b9f63c";
-
-function getWeather() {
-    var weatherUrl = `https://api.weatherbit.io/v2.0/current/?city=${city}&key=${apiKey}&units=i`
-
-    $.ajax({
-        url: weatherUrl,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        var current = response.data[0]
-
-        //current
-        var cityName = $(`<p> ${current.city_name} </p>`);
-        var temp = $(`<p> Current Temperature: ${current.temp} &degF </p> `);
-        //    var icon = $(`<img src="${current.condition.icon}"`);
-        var wind = $(`<p> Wind Speed: ${current.wind_spd} MPH</p>`);
-
-        $("#current").append(cityName, temp, wind);
-    })
-
-}
-
-function forecast() {
-    var forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${apiKey}&units=i&days=3`
-
-    $.ajax({
-        url: forecastUrl,
-        method: "GET"
-    }).then(function (response) {
-
-        var forecast = response.data
-
-        for (var i = 0; i < forecast.length; i++) {
-            // var cityName = $(`<p> ${forecast.city_name} </p>`);
-            var foreTemp = $(`<p> Current Temperature: ${forecast[i].temp} &degF </p> `);
-            //    var icon = $(`<img src="${current.condition.icon}"`);
-            var foreWind = $(`<p> Wind Speed: ${forecast[i].wind_spd} MPH</p>`);
-            $("#forecast").append(foreTemp,foreWind);
-        }
-
-    })
-
-}
-
-
-    $("#search").on("click", function () {
-        city = $("#search-input").val().trim();
-        getWeather();
-        forecast();
-        console.log(city)
-    })
-
+var lon;
+var lat;
 var userInput;
 
 
@@ -99,7 +51,69 @@ $("#add-park").on("click", function (event) {
     event.preventDefault();
     userInput = $("#user-input").val().trim();
     console.log(userInput);
-
+   
     stateParks();
     $("#user-input").val("");
 });
+
+// ?? on click event for info block
+// ---- populate lon and lat (use response.data.latitude and response.data.longitude)
+// --- call weather functions
+
+// ------------- WEATHER FUNCTIONS ----------------------
+
+function getWeather() {
+    //!update this to input lat and lon parameters
+    var weatherUrl = `https://api.weatherbit.io/v2.0/current/?city=denver&key=${apiKey}&units=i`
+
+    $.ajax({
+        url: weatherUrl,
+        method: "GET"
+    }).then(function (response) {
+       
+        var current = response.data[0]
+        
+        //current
+        var cityName = $(`<p> ${current.city_name} </p>`);
+        var temp = $(`<p> Current Temperature: ${current.temp} &degF </p> `);
+        var iconCode = current.weather.icon
+        var icon = $(`<img>`)
+        icon.attr("src", `assets/icons/${iconCode}.png`)
+        var wind = $(`<p> Wind Speed: ${current.wind_spd} MPH</p>`);
+
+        $("#current").prepend(cityName, icon, temp, wind);
+    })
+
+}
+
+function forecast() {
+    //!update this to input lat and lon parameters
+    var forecastUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=denver&key=${apiKey}&units=i&days=3`
+
+    $.ajax({
+        url: forecastUrl,
+        method: "GET"
+    }).then(function (response) {
+
+        var forecast = response.data
+       
+        for (var i = 0; i < forecast.length; i++) {
+            var foreDate = $(`<p> ${moment.unix(forecast[i].ts).format("M/D/YY")} </p> `);
+            var foreTemp = $(`<p> Current Temperature: ${forecast[i].temp} &degF </p> `);
+            var iconCode = forecast[i].weather.icon
+            var icon = $(`<img>`)
+            icon.attr("src", `assets/icons/${iconCode}.png`)
+            var foreWind = $(`<p> Wind Speed: ${forecast[i].wind_spd} MPH</p>`);
+            $("#forecast").prepend(foreDate, icon, foreTemp, foreWind);
+        }
+
+    })
+
+}
+
+
+    $("#search").on("click", function () {
+        city = $("#search-input").val().trim();
+        getWeather();
+        forecast();
+    })

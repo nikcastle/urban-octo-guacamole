69aa9832860c95a9b95ed19915f860985e843c3e
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-07-13 17:35:44 -0600

    updating code

--- script.js
+++ script.js
@@ -26,6 +26,15 @@ function clearParkInfo() {
     $("#fullName").empty();
 }
 
+function autoComplete() {
+
+    var stateCodes = ("AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY").split(" ");
+    $( "#user-input" ).autocomplete({
+        source: stateCodes
+    });
+    
+}
+
 // when page loads
 function onLoad() {
     $("#appInfo").show();
@@ -34,6 +43,7 @@ function onLoad() {
     $("#goBack").hide();
     $("#progressbar").hide();
     clearStorage();
+    autoComplete();
 }
 
 
